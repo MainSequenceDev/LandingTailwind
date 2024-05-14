2bb3b1486e3a9f327cdbe7ce9d471bfbe4539f29
Abdülfettah Adwani <fettahaud@gmail.com>
2024-05-14 11:12:27 +0300

    edit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#8ab4f8",
+    },
+  },
+  plugins: [],
+};
